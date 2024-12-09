4687fc559f857b7bb9384c143af976b2cba833d7
Inkhermit <lisongwei200110@gmail.com>
2024-12-09 11:22:23 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import TermPage from './components/TermPage';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
 
-const dataUrl = "https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php";
-
 const queryClient = new QueryClient();
 
 const App = () => {
